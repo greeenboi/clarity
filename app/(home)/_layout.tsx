f17605f3bd4b60194335ee8dc8fe87a6de4eb113
greeenboi <suvan.gowrishanker.204@gmail.com>
2024-08-07 00:28:06 +0530

    signing out bug fixes

--- app/(home)/_layout.tsx
+++ app/(home)/_layout.tsx
@@ -1,13 +1,6 @@
-import { Stack, Redirect } from "expo-router";
-
-import { useAuthContext } from "~/providers/AuthContext";
+import { Stack } from "expo-router";
 
 export default function HomeLayout() {
-  const { session } = useAuthContext();
-  if (!session || !session.user) {
-    console.log("Redirecting to sign-in");
-    return <Redirect href="/" />;
-  }
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
--- components/auth/sign-out.tsx
+++ components/auth/sign-out.tsx
@@ -7,26 +7,34 @@ import { Spinner } from "tamagui";
 import { Logout } from "../icons";
 
 
+import { useAuthContext } from "~/providers/AuthContext";
 import { Button } from "~/tamagui.config";
 import { supabase } from "~/utils/supabase";
 
 export default function SignOutButton () {
     const [status, setStatus] = useState<'off' | 'submitting' | 'submitted'>('off');
+    const { session } = useAuthContext();
     const SignOut = async () => {
+        console.log(session);
+        console.log("signing out");
         setStatus('submitting');
-        const { error } = await supabase.auth.signOut();
+        const { error } = await supabase.auth.signOut({
+            scope:'local'
+        });
         if (error) {
             ToastAndroid.show(error.message, ToastAndroid.SHORT);
+            console.log(error.message);
             setStatus('off');
         } else {
             ToastAndroid.show("Signed out", ToastAndroid.SHORT);
             console.log("signed out");
-            router.replace("/");
+            router.replace("/sign-in");
             setStatus('submitted');
         }
+        
       };
     return(
-        <Button icon={status === 'submitting' ? () => <Spinner /> : Logout} backgroundColor='$color.error' onPress={() => SignOut()} disabled={status !== 'off'}>
+        <Button icon={status === 'submitting' ? () => <Spinner /> : Logout} backgroundColor='$color.error' onPress={() => SignOut()} >
             Sign Out
         </Button>
     )
